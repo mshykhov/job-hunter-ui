fdadd7104131e878f87da79ab76ced4349fd89df
mshykhov <myronshykhov@gmail.com>
2026-02-25 12:30:30 +0800

    fix: disable sort tooltip on table columns

--- src/features/jobs/components/JobTable.tsx
+++ src/features/jobs/components/JobTable.tsx
@@ -88,6 +88,7 @@ const ALL_COLUMNS: ColumnsType<Job> = [
     dataIndex: "publishedAt",
     width: 100,
     sorter: (a, b) => (a.publishedAt ?? "").localeCompare(b.publishedAt ?? ""),
+    showSorterTooltip: false,
     render: formatRelativeDate,
   },
   {
@@ -96,6 +97,7 @@ const ALL_COLUMNS: ColumnsType<Job> = [
     dataIndex: "matchedAt",
     width: 100,
     sorter: (a, b) => (a.matchedAt ?? "").localeCompare(b.matchedAt ?? ""),
+    showSorterTooltip: false,
     render: formatRelativeDate,
   },
 ];
